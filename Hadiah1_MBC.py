c9b868a79639225d7045ef58cc13a150c23afc9a
bananasur <127819637+bananasur@users.noreply.github.com>
2023-07-18 16:53:28 +0700

    Update Hadiah1_MBC.py

--- Hadiah1_MBC.py
+++ Hadiah1_MBC.py
@@ -1,28 +1,32 @@
 print ("## Program Python Diskon Potongan Harga ##")
 print ("==========================================")
 
-total_belanja = int(input("Total Belanja: "))
+try:
+    total_belanja = int(input("Total Belanja: "))
+    if total_belanja < 0:
+            raise ValueError("Total belanja tidak boleh negatif!")
+    
+    if total_belanja < 100000:
+        diskon = 0
+        x= total_belanja - (total_belanja * diskon) 
+        print ("Selamat, anda mendapatkan diskon {}%".format(diskon))
+        print ("Total bayar: Rp.{}".format(x))
+    elif total_belanja <= 500000:
+        diskon = 0.1
+        x= total_belanja - (total_belanja * diskon) 
+        print ("Selamat, anda mendapatkan diskon 10%")
+        print ("Total bayar: Rp.{}".format(x))
+    elif total_belanja <= 1000000:
+        diskon = 0.2
+        x= total_belanja - (total_belanja * diskon) 
+        print ("Selamat, anda mendapatkan diskon 20%")
+        print ("Total bayar: Rp.{}".format(x))
+    else:
+        diskon = 0.3
+        x = total_belanja - (total_belanja * diskon) 
+        print ("Selamat, anda mendapatkan diskon 30%")
+        print ("Total bayar: Rp.{}".format(x))
 
-if total_belanja < 100000:
-    diskon = 0
-    x= total_belanja - (total_belanja * diskon) 
-    print ("Selamat, anda mendapatkan diskon {}%".format(diskon))
-    print ("Total bayar: Rp.{}".format(x))
-elif total_belanja <= 500000:
-    diskon = 0.1
-    x= total_belanja - (total_belanja * diskon) 
-    print ("Selamat, anda mendapatkan diskon 10%")
-    print ("Total bayar: Rp.{}".format(x))
-elif total_belanja <= 1000000:
-    diskon = 0.2
-    x= total_belanja - (total_belanja * diskon) 
-    print ("Selamat, anda mendapatkan diskon 20%")
-    print ("Total bayar: Rp.{}".format(x))
-else:
-    diskon = 0.3
-    x = total_belanja - (total_belanja * diskon) 
-    print ("Selamat, anda mendapatkan diskon 30%")
-    print ("Total bayar: Rp.{}".format(x))
-
-
+except ValueError as e:
+    print("Terjadi kesalahan:", str(e))
 
